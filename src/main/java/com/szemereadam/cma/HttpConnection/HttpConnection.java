1bc0ee130e2ebbe82fe7375d4911471d6e073796
Kimyan099 <sephxdnb@gmail.com>
2021-02-23 20:40:02 +0100

    Update error handling

--- src/main/java/com/szemereadam/cma/HttpConnection/HttpConnection.java
+++ src/main/java/com/szemereadam/cma/HttpConnection/HttpConnection.java
@@ -1,6 +1,8 @@
 package com.szemereadam.cma.HttpConnection;
 
 import com.szemereadam.cma.Logger.ExceptionLog;
+import org.json.JSONException;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class HttpConnection {
     private final ExceptionLog exceptionLog = new ExceptionLog();
 
-    public String getContent(String api) {
+    public String getContent(String api) throws JSONException {
         RestTemplate restTemplate = new RestTemplate();
         try {
             return restTemplate.getForObject(api, String.class);
@@ -17,6 +19,10 @@ public class HttpConnection {
         } catch (RestClientException e) {
             exceptionLog.log(e);
             throw new RestClientException("Request fails because of server response error");
+
+        }catch (JsonParseException e) {
+            exceptionLog.log(e);
+            throw new JSONException("Json can not be parsed");
 
         } catch (IllegalArgumentException e) {
             exceptionLog.log(e);
