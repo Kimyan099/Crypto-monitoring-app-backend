19ef38e37ca26cfc1218476a2d96378770a55c9e
Kimyan099 <sephxdnb@gmail.com>
2021-02-23 22:02:01 +0100

    Remove unused imports from NewsRepository test

--- src/test/java/com/szemereadam/cma/repository/NewsRepositoryTest.java
+++ src/test/java/com/szemereadam/cma/repository/NewsRepositoryTest.java
@@ -4,8 +4,6 @@ import com.szemereadam.cma.model.News;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
