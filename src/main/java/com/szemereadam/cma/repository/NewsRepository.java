1184e45780fe0183dad1158a70a3cc1020bcee9f
Kimyan099 <sephxdnb@gmail.com>
2021-02-28 20:02:41 +0100

    Update NewsRepository with Asc and Desc filter

--- src/main/java/com/szemereadam/cma/repository/NewsRepository.java
+++ src/main/java/com/szemereadam/cma/repository/NewsRepository.java
@@ -2,6 +2,18 @@ package com.szemereadam.cma.repository;
 
 import com.szemereadam.cma.model.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+    @Query("FROM News ORDER BY publishedAt DESC")
+    @Modifying(clearAutomatically = true)
+    List<News> getAllNewsDESC();
+
+    @Query("FROM News ORDER BY publishedAt ASC")
+    @Modifying(clearAutomatically = true)
+    List<News> getAllNewsASC();
 }
